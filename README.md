6dc6c8cfec376f2790c7ef03cfa016711fb18e98
khanMohsinn <mohsin.khan@habilelabs.io>
2022-04-07 16:15:28 +0530

    Added video streaming using fs and express

--- README.md
+++
@@ -1,2 +0,0 @@
-# A simple video streaming application
-A node js program which utilizes the fs module to stream a video on the local nodejs server.
--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-import animal from "botanic-zoo-api";
+import express from "express";
+import path from "path";
 
-animal
-  .getAnimal("panther") //Here we can give an animal's name as the argument.
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+const __dirname = path.resolve();
+const app = express();
+import { statSync, createReadStream } from "fs";
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/video", function (req, res) {
+  // Ensure there is a range given for the video
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires Range header");
+  }
+
+  const videoPath = "Cake8Door.mp4";
+  const videoSize = statSync("Cake8Door.mp4").size;
+  const CHUNK_SIZE = 10 ** 5; // 0.1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  // Create headers
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers);
+
+  // create video read stream for this particular chunk
+  const videoStream = createReadStream(videoPath, { start, end });
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res);
+});
+
+app.listen(8000, function () {
+  console.log("Listening on port 8000!");
+});
