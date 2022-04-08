720eb446668aa8c0946a1a5fcd747fc1a316ce91
Mohsin Khan <102362411+khanMohsinn@users.noreply.github.com>
2022-04-08 11:55:23 +0530

    Revert "A Simple REST API created using Express for a Node Server"

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-import express from "express";
+import animal from "botanic-zoo-api";
 
-const app = express();
-
-app.use(express.json());
-/* We parse the body of the request to 
-JSON before handling the request. This is knows as MIDDLEWARE */
-
-const PORT = 8080;
-app.listen(PORT, () => console.log(`it's alive on localhost:${PORT}`));
-
-app.get("/shoes", (req, res) => {
-  res.status(200).send({
-    shoes: "👟",
-    size: "UK-9",
-  });
-});
-
-app.post("/shoes/:id", (req, res) => {
-  const { id } = req.params;
-  const { logo } = req.body;
-
-  if (!logo) {
-    res.status(418).send({
-      message: "We need a logo",
-    });
-  }
-
-  res.send({
-    shoes: `👟 with your ${logo} and id of ${id}`,
-  });
-});
+animal
+  .getAnimal("panther") //Here we can give an animal's name as the argument.
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
